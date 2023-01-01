66e591e3e20a959054e8dd312e27532b6ad5dcd6
Whyudin14 <wahy21140ti@student.nurulfikri.ac.id>
2023-01-01 15:19:35 +0700

    Menambahkan file

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -1,92 +1,127 @@
-// TODO 3: Import data students dari folder data/students.js
-// code here
-// const students = require("../data/students");
-
-// import class StudentController
+// import model student
 const Student = require("../models/Student");
+
 
 // Membuat Class StudentController
 class StudentController {
-  // index(req, res) {
-    
-  //   // TODO 4: Tampilkan data students
-  //   // code here
-  //   // memanggil method static all
-  //   Student.all(function (students){
-  //     const data = {
-  //       message: "Menampilkan semua students",
-  //       data: students,
-  //     };
+  // menambah keyword async
+        async index(req, res) {
+
+          // Memanggil method static all dengan async await
+          const students = await Student.all();
+
+          // jika data array lebih dari 0
+          if(students.lenght > 0){
+            const data = {
+              message: "Menampilkan semua students",
+              data: students,
+              };
+              return res.status(200).json(data);
+          }
+          // else
+          const data = {
+            message: "Student is empyty",
+            };
+            return res.status(200).json(data);
+        };
+
+        async store(req, res) {
+          // const {nama} = req.body;
+
+          /**
+           * Validasi sederhana
+           * menghandle jika salah satu data tidak di kirim
+           */
+
+          // destructing object req.body
+          const{nama, nim, email, jurusan} = req.body;
+
+          // jika data undefined maka kirim response eror
+          if(!nama || !nim || !email || !jurusan){
+            const data = {
+              message: "Semua data harus dikirim",
+            };
   
-  //     res.json(data);
-  //   });
-  // }
+            return res.status(422).json(data);
+          }
+          // else
+          // Memanggil method static create dengan async await
+          const students = await Student.create(req.body);
+          
+          const data = {
+            message: `Menambahkan data student`,
+            data: students,
+          };
 
-  // menambahkan keyword async
-  async index(req, res) {
-    
-    // TODO 4: Tampilkan data students
-    // code here
-    // memanggil method static dengan async await
-    const students = await Student.all();
+          return res.status(201).json(data);
+        };
 
-      const data = {
-        message: "Menampilkan semua students",
-        data: students,
-      };
-  
-      res.json(data);
-  }
+        async update(req, res) {
+          const { id } = req.params;
+          // const { nama } = req.body;
+          // students[id] = nama;
+          // mencari id student yang ingin di update
+          const student = await Student.find(id);
 
-  async store(req, res) {
-    // const {nama} = req.body;
+          if(student){
+            // melakukan update data
+            const student = await Student.update(id, req.body);
+            const data = {
+              message: `Mengedit data students`,
+              data: student,
+            };
+            res.status(200).json(data);
+          }
+          else{
+            const data = {
+              message:`Student not found`,
+            };
+            res.status(404).json(data);
+          }
+        }
+        
+        async destroy(req, res) {
+          const { id } = req.params;
+          const student = await Student.find(id);
 
-    // Memanggil method static create dengan async await
-    const students = await Student.create(req.body);
+          if(student){
+            // melakukan hapus data
+            await Student.delete(id);
+            const data = {
+              message: `Menghapus data students`,
+            };
+            res.status(200).json(data);
+          }
+          else{
+            const data = {
+              message:`Student not found`,
+            };
+            res.status(404).json(data);
+          }
+        }
 
-    const data = {
-      message: `Menambahkan data student`,
-      data: students,
-    };
+        async show(req, res) {
+          const { id } = req.params;
+          // mencari id student yang ingin di update
+          const student = await Student.find(id);
 
-    res.json(data);
-  };
-
+          if(student){
+            const data = {
+              message: `Menampilkan data students`,
+              data: student,
+            };
+            res.status(200).json(data);
+          }
+          else{
+            const data = {
+              message:`Student not found`,
+            };
+            res.status(404).json(data);
+          }
+        }
 }
-
-//   update(req, res) {
-//     const { id } = req.params;
-//     const { nama } = req.body;
-//     students[id] = nama;
-
-//     // TODO 6: Update data students
-//     // code here
-//     const data = {
-//       message: `Mengedit student id ${id}, nama ${nama}`,
-//       data: students,
-//     };
-
-//     res.json(data);
-//   }
-
-//   destroy(req, res) {
-//     const { id } = req.params;
-//     students.splice(id,1)
-
-//     // TODO 7: Hapus data students
-//     // code here
-//     const data = {
-//       message: `Menghapus student id ${id}`,
-//       data: students,
-//     };
-
-//     res.json(data);
-//   }
-
-
 // Membuat object StudentController
 const object = new StudentController();
 
 // Export object StudentController
 module.exports = object;
-
--- models/Student.js
+++ models/Student.js
@@ -23,15 +23,48 @@ class Student{
         });
     }
 
-    static create(req){
-        return new Promise((resolve, reject)=>{
-            const sql = `INSERT INTO students set?`;
+    static async create(data){
+        // Promise 1 : Melakukan insert data ke database
+        const id = await new Promise((resolve, reject) => {
+            const sql = "INSERT INTO students SET ?";
+            db.query(sql,data,(err,results)=>{
+                resolve(results.insertId);
+            });
+        });
+        // Refactor promise 2 : get data by id
+        const student = this.find(id);
+        return student;
+    }
 
-            db.query(sql,req,(err,results)=>{
-                const sql = `SELECT * FROM students WHERE id = ${results.insertId}`;
-                db.query(sql,(err,results)=>{
-                    resolve(results);
-                });
+    static find(id){
+        return new Promise((resolve,reject) => {
+            const sql = "SELECT * FROM students WHERE id = ?";
+            db.query(sql,id,(err,results) => {
+                // destructing array
+                const [student] = results;
+                resolve(student);
+            });
+        });
+    }
+
+    static async update(id,data){
+        // return promise sebagai solusi asynchronous
+        await new Promise((resolve, reject)=>{
+            const sql = "UPDATE students SET ? WHERE id = ?";
+            db.query(sql,[data,id],(err,results) => {
+                resolve(results);
+            });
+        });
+
+        const student = await this.find(id);
+        return student;
+    }
+
+    static delete(id){
+        return new Promise((resolve,reject) => {
+            const sql = "DELETE FROM students WHERE id = ?";
+            db.query(sql, id, (err,results) => {
+                resolve(results);
             });
         });
     }
